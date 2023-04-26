5d92b3cc21e7fe833840f7ec3204638e7e9e0e90
wcasttro <wcastro.santos10@gmail.com>
2023-04-25 22:03:51 -0300

    adicionando codigo para abrir app Flutter

--- app_swift_ui_test_integration/app_swift_ui_test_integration/ContentView.swift
+++ app_swift_ui_test_integration/app_swift_ui_test_integration/ContentView.swift
@@ -17,13 +17,45 @@
 
 // 4º usar pod install e pod update
 
+// 5º fechar projeto e abrir o arquivo xcworkspace
+
+// 6º adicionar codigo para abrir app Flutter
+
+// 7º executar o projeto mesmo que tenha erros aparentes, eles vão sumir após o builder
+
 import SwiftUI
+import Flutter
+
 
 struct ContentView: View {
+    
+    @EnvironmentObject var flutterDependencies: FlutterDependencies
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Open FlutterApp") {
+             openFlutterApp()
+           }
     }
+    
+    func openFlutterApp() {
+        // Get the RootViewController.
+        guard
+          let windowScene = UIApplication.shared.connectedScenes
+            .first(where: { $0.activationState == .foregroundActive && $0 is UIWindowScene }) as? UIWindowScene,
+          let window = windowScene.windows.first(where: \.isKeyWindow),
+          let rootViewController = window.rootViewController
+        else { return }
+
+        // Create the FlutterViewController without an existing FlutterEngine.
+        let flutterViewController = FlutterViewController(
+          project: nil,
+          nibName: nil,
+          bundle: nil)
+        flutterViewController.modalPresentationStyle = .overCurrentContext
+        flutterViewController.isViewOpaque = false
+
+        rootViewController.present(flutterViewController, animated: true)
+      }
 }
 
 struct ContentView_Previews: PreviewProvider {
--- app_swift_ui_test_integration/app_swift_ui_test_integration/app_swift_ui_test_integrationApp.swift
+++ app_swift_ui_test_integration/app_swift_ui_test_integration/app_swift_ui_test_integrationApp.swift
@@ -6,6 +6,19 @@
 //
 
 import SwiftUI
+import Flutter
+
+import FlutterPluginRegistrant
+
+class FlutterDependencies: ObservableObject {
+  let flutterEngine = FlutterEngine(name: "my flutter engine")
+  init(){
+    // Runs the default Dart entrypoint with a default Flutter route.
+    flutterEngine.run()
+    // Connects plugins with iOS platform code to this app.
+    GeneratedPluginRegistrant.register(with: self.flutterEngine);
+  }
+}
 
 @main
 struct app_swift_ui_test_integrationApp: App {
