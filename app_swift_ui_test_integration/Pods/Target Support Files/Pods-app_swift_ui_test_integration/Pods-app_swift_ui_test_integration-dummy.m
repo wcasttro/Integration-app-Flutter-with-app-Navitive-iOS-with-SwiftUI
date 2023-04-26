66a17b13337a17fe174d7fcd7aadbd44c3a38b4e
wcasttro <wcastro.santos10@gmail.com>
2023-04-25 21:50:00 -0300

    criando e alterando podfile

---
+++ app_swift_ui_test_integration/Pods/Target Support Files/Pods-app_swift_ui_test_integration/Pods-app_swift_ui_test_integration-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_app_swift_ui_test_integration : NSObject
+@end
+@implementation PodsDummy_Pods_app_swift_ui_test_integration
+@end
--- app_swift_ui_test_integration/app_swift_ui_test_integration/ContentView.swift
+++ app_swift_ui_test_integration/app_swift_ui_test_integration/ContentView.swift
@@ -10,6 +10,12 @@
 // 1º criar e adicionar modulo Flutter na pasta principal do projeto
 // comnando para criar modulo: flutter create -t module <module_name>
 
+// 2º criar podfile, comando: pod init
+
+// 3º abrir pelo xcode e adicionar codigo ao podfile, na parte superior após o use_frameworks!
+// obs:  adicionar install_all_flutter_pods(flutter_application_path). para cada target
+
+// 4º usar pod install e pod update
 
 import SwiftUI
 
