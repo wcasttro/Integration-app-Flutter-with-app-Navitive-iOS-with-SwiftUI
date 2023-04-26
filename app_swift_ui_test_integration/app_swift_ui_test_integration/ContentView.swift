66ade78dcae5f6101d31301d93414ef0ec507025
wcasttro <wcastro.santos10@gmail.com>
2023-04-25 21:35:37 -0300

    iniciando projeto swiftUI

---
+++ app_swift_ui_test_integration/app_swift_ui_test_integration/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  app_swift_ui_test_integration
+//
+//  Created by werthen castro on 25/04/23.
+//
+
+// Passos para a integração
+
+// 1
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ app_swift_ui_test_integration/app_swift_ui_test_integration/app_swift_ui_test_integrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_swift_ui_test_integrationApp.swift
+//  app_swift_ui_test_integration
+//
+//  Created by werthen castro on 25/04/23.
+//
+
+import SwiftUI
+
+@main
+struct app_swift_ui_test_integrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
